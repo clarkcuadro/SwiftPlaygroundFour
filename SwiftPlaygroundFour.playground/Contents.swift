093a358f479be1a37f8cb406525e888af0553bbb
tafitzs <35237432+tafitzs@users.noreply.github.com>
2018-02-28 15:35:01 -1000

    Assignment #6 commit and push

--- SwiftPlaygroundFour.playground/Contents.swift
+++ SwiftPlaygroundFour.playground/Contents.swift
@@ -30,3 +30,33 @@ while i <= 10
         //we want to add + 1 to i for each iteration through the loop
         i = i + 1
 }
+
+//Coding Challenge #1
+var e = 1
+while e <= 3
+{
+    //we want to print i
+    print(e)
+    //we want to add + 1 to i for each iteration through the loop
+    e = e + 1
+}
+
+//Coding Challenge #2
+
+for number in 1...10
+{
+    print(number)
+}
+
+
+
+var newSum = 0
+for number in 1...10
+{
+    sum -= number
+    print(sum)
+}
+
+
+
+
